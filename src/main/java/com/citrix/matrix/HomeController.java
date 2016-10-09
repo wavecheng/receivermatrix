5d635fcfa13d529a1c59c0d129fa1cf07d9b5d67
Bo Chen <bo.chen@citrix.com>
2016-10-09 13:29:54 +0800

    integrate with h2 and REST API load/save operation

--- src/main/java/com/citrix/matrix/HomeController.java
+++ src/main/java/com/citrix/matrix/HomeController.java
@@ -1,14 +1,43 @@
 package com.citrix.matrix;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.citrix.matrix.model.Cell;
+import com.citrix.matrix.repository.CellRepository;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private CellRepository CellRepository;
+	
+	private Logger log = LoggerFactory.getLogger("matrix");
 	@RequestMapping("/")
 	public String index(){
 		return "index";
 	}
+	
+	@RequestMapping(value="/save", method=RequestMethod.POST)
+	@ResponseBody
+	public Cell save(@RequestBody Cell cell){
+		log.debug(cell.toString());
+		cell = CellRepository.save(cell);
+		return cell;
+	}
+	
+	@RequestMapping(value="/load", method=RequestMethod.GET)
+	@ResponseBody
+	public List<Cell> load(){
+		return CellRepository.findAll();
+	}	
 	
 }
---
+++ src/main/java/com/citrix/matrix/model/Cell.java
@@ -0,0 +1,46 @@
+package com.citrix.matrix.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name="Matrix")
+public class Cell {
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private int row;
+	private int column;
+	private String value;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public int getColumn() {
+		return column;
+	}
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%1$d [%2$d,%3$d]=%4$s", id,row,column,value);
+	}
+}
