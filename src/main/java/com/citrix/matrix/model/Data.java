3520caf1175f7b989e6f149eec699c232b25ec8e
Bo Chen <bo.chen@citrix.com>
2016-10-18 12:41:26 +0800

    user CLOB for data storage

--- src/main/java/com/citrix/matrix/model/Data.java
+++ src/main/java/com/citrix/matrix/model/Data.java
@@ -1,14 +1,18 @@
 package com.citrix.matrix.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 
 @Entity
 public class Data {
 	@Id
 	private long id = 1;
 	
+	@Column
+	@Lob
 	private String tableData;
 
 	public long getId() {
